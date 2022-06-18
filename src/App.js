59e6b2366ba9e409172fadb21b99fa548ab5f320
Davit <davit@Davits-MacBook-Pro.local>
2022-06-18 23:50:57 +0400

    just commit all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(1);
+    return (
+        <div className="App">
+            <div className="container">
+                <span
+                    className="plusMinus"
+                    onClick={() => count > 1 && setCount(count - 1)}
+                >
+                    -
+                </span>
+                <div className="round">{count}</div>
+                <span className="plusMinus" onClick={() => setCount(count + 1)}>
+                    +
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default App;
